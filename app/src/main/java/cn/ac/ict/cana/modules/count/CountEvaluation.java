06cbe1bfb85342310d24d22b780563e656e69820
saukymo <saukymo@gmail.com>
2016-09-28 14:10:24 +0800

    Evaluation interface example.

---
+++ app/src/main/java/cn/ac/ict/cana/modules/count/CountEvaluation.java
@@ -0,0 +1,21 @@
+package cn.ac.ict.cana.modules.count;
+
+import cn.ac.ict.cana.models.History;
+
+/**
+ * Author: saukymo
+ * Date: 9/28/16
+ */
+
+public class CountEvaluation {
+    public CountEvaluation() {
+
+    }
+
+    static public String evaluation(History history){
+
+//        Do something here.
+
+        return "Example evaluation string";
+    }
+}
